11621cc2499bcfd075dddd9fe6ba6230e10d57d3
tjslzz <3244188372@qq.com>
2019-08-09 14:48:14 +0800

    [jerryLi] build : build project about tdd work on 2019.08.09

---
+++ src/test/java/TddWorkTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TddWorkTest {
+
+    @Test
+    public void should_return_3D_when_call_judges_given_2H_and_3D(){
+
+    }
+}
