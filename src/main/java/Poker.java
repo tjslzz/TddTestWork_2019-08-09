907575a225c39f9842c872ecaeba8de2900b9355
JarryLi <3244188372@qq.com>
2019-08-10 10:44:41 +0800

    [jerryLi] feat : finish word compare and pass the test

--- src/main/java/Poker.java
+++ src/main/java/Poker.java
@@ -2,42 +2,59 @@ public class Poker {
     private PokerColor color;
     private String number;
 
-    public Poker() {
-    }
-
-    public Poker(PokerColor color, String number) {
+    protected Poker(PokerColor color, String number) {
         this.color = color;
         this.number = number;
     }
 
-    public PokerColor getColor() {
-        return color;
-    }
 
-    public void setColor(PokerColor color) {
-        this.color = color;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String toString(){
-        return this.number+this.color;
-    }
-
-    public String compareTo(Poker poker){
-        if(Character.isDigit(this.number.charAt(0)) && Character.isDigit(poker.getNumber().charAt(0))){
-            if(Integer.valueOf(this.number) > Integer.valueOf(poker.getNumber()))
-                return this.toString();
-            else if(Integer.valueOf(this.number) == Integer.valueOf(poker.getNumber()))
-                return "平局";
+    protected String compareTo(Poker poker) {
+        if (Character.isDigit(this.number.charAt(0)) && Character.isDigit(poker.number.charAt(0))) {
+            if (Integer.valueOf(this.number) > Integer.valueOf(poker.number)) return this.toString();
+            else if (Integer.valueOf(this.number) == Integer.valueOf(poker.number)) return "平局";
+            return poker.toString();
+        } else {
+            this.number = setNumber(this);
+            poker.number = setNumber(poker);
+            if (Integer.valueOf(this.number) > Integer.valueOf(poker.number)) return this.toString();
+            else if (Integer.valueOf(this.number) == Integer.valueOf(poker.number)) return "平局";
             return poker.toString();
         }
-        return null;
+    }
+
+    public String toString() {
+        return this.getNumber() + this.color;
+    }
+
+    private String setNumber(Poker poker) {
+        switch (poker.number) {
+            case "T":
+                return "11";
+            case "J":
+                return "12";
+            case "Q":
+                return "13";
+            case "K":
+                return "14";
+            case "A":
+                return "15";
+        }
+        return poker.number;
+    }
+
+    private String getNumber() {
+        switch (number) {
+            case "11":
+                return "T";
+            case "12":
+                return "J";
+            case "13":
+                return "Q";
+            case "14":
+                return "K";
+            case "15":
+                return "A";
+        }
+        return number;
     }
 }
