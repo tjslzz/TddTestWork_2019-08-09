6bcd921c1acd9f18b582712a0972c8fe9ea2fe08
tjslzz <3244188372@qq.com>
2019-08-10 17:41:58 +0800

    [jerryLi] feat : add level 7 func and pass the test

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,6 +7,7 @@ public class User {
     private List<Poker> pokers;
     private static final int IS_POKER_SIZE = 5;
     private static final int IS_ONE_PAIR = 4;
+    private static final int IS_FOUR_KIND = 4;
     private static final int IS_THREE_KIND = 3;
     private static final int IS_FULL_HOUSE = 2;
     private static final Long IS_FLUSH = 1L;
@@ -17,6 +18,7 @@ public class User {
     private static final int THREE_KIND_LEVEL = 4;
     private static final int FLUSH_LEVEL = 5;
     private static final int FULL_HOUSE_LEVEL = 6;
+    private static final int FOUR_KIND_LEVEL = 7;
 
     public User(String userName, List<Poker> pokers) {
         this.userName = userName;
@@ -70,7 +72,7 @@ public class User {
             case IS_ONE_PAIR:
                 return PAIR_LEVEL;
             case IS_FULL_HOUSE:
-                return FULL_HOUSE_LEVEL;
+                return max == IS_FOUR_KIND ? FOUR_KIND_LEVEL : FULL_HOUSE_LEVEL;
         }
         return max == IS_THREE_KIND ? THREE_KIND_LEVEL : TWO_PAIR_LEVEL;
     }
