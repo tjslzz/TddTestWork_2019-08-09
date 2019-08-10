c6261591629f602e08d55bc5c4d22db8585507ce
tjslzz <3244188372@qq.com>
2019-08-10 16:46:17 +0800

    [jerryLi] feat : add func of level 4 and pass the test

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,9 +7,11 @@ public class User {
     private List<Poker> pokers;
     private static final Long COUNT_PAIR = 4L;
     private static final Long COUNT_TWO_PAIR = 3L;
+    private static final int COUNT_THREE_KIND = 3;
     private static final int HIGH_CARD_LEVEL = 1;
     private static final int PAIR_LEVEL = 2;
     private static final int TWO_PAIR_LEVEL = 3;
+    private static final int THREE_KIND_LEVEL = 4;
 
     public User(String userName, List<Poker> pokers) {
         this.userName = userName;
@@ -29,6 +31,7 @@ public class User {
     }
 
     public int getMyLevel() {
+        if (isThreeKind()) return THREE_KIND_LEVEL;
         if (isTwoPair()) return TWO_PAIR_LEVEL;
         if (isPair()) return PAIR_LEVEL;
         return HIGH_CARD_LEVEL;
@@ -42,5 +45,18 @@ public class User {
     private Boolean isTwoPair() {
         List<String> list = pokers.stream().map(poker -> poker.getNumber()).collect(Collectors.toList());
         return list.stream().distinct().count() == COUNT_TWO_PAIR;
+    }
+
+    private Boolean isThreeKind() {
+        int max = 1;
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < pokers.size(); i++) {
+            if (list.contains(pokers.get(i).getNumber())) {
+                max++;
+            } else {
+                list.add(pokers.get(i).getNumber());
+            }
+        }
+        return max == COUNT_THREE_KIND;
     }
 }
