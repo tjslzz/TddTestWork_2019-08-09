ac0b2df46c3fb6b9df333f737a0892d8f15ac4e7
JarryLi <3244188372@qq.com>
2019-08-11 20:45:13 +0800

    [jerryLi] feat : add new func of compare same level 3

--- src/main/java/TddWork.java
+++ src/main/java/TddWork.java
@@ -3,14 +3,16 @@ public class TddWork {
         if (user1.getMyLevel() == user2.getMyLevel()) {
             switch (user1.getMyLevel()) {
                 case Common.PAIR_LEVEL:
-                    return handleLevel2(user1, user2);
+                    return handlePairLevel(user1, user2);
+                case Common.TWO_PAIR_LEVEL:
+                    return handlePairLevel(user1, user2);
             }
             return user1.getMyMaxPoker().compareTo(user2.getMyMaxPoker()).equals(user1.getMyMaxPoker()) ? user1 : user2;
         }
         return user1.getMyLevel() > user2.getMyLevel() ? user1 : user2;
     }
 
-    private User handleLevel2(User user1, User user2) {
+    private User handlePairLevel(User user1, User user2) {
         return user1.getMaxPoker() > user2.getMaxPoker() ? user1 : user2;
     }
 }
